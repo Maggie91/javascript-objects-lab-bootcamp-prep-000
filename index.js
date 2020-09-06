02d41e89f854dbc8f4b7cf16a47f658da1934ba2
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-06 08:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {};
    - returns an object with an updated key value pair */
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  return Object.assign({}, object, { [key]: value })
   }
 
 /* - updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object */
